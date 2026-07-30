cb0322a5c3aab386874336f603aaf5be5ed5f50f
Jiseong <pupajusang01@gmail.com>
2026-07-30 19:26:15 +0900

    chore: init project

---
+++ src/main/java/cloud/anzaanza/antiagingdna/AntiagingdnaApplication.java
@@ -0,0 +1,13 @@
+package cloud.anzaanza.antiagingdna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiagingdnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiagingdnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloud/anzaanza/antiagingdna/AntiagingdnaApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.anzaanza.antiagingdna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiagingdnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
